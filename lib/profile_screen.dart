d321a408ba6279f26922518ddcdaf6333a9d72df
akram-tello <a.tello@graduate.utm.my>
2022-05-29 15:14:31 +0800

    refactor profile-screen

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,25 +1,7 @@
-// import 'package:flutter/material.dart';
-// class ProfileScreen extends StatefulWidget {
-//   const ProfileScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   State<ProfileScreen> createState() => _ProfileScreenState();
-// }
-//
-// class _ProfileScreenState extends State<ProfileScreen> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Text("welcome to your profile page"),
-//       ),
-//
-//     );
-//   }
-// }
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:signfun/main21.dart';
+
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -32,33 +14,27 @@ class ProfileScreen extends StatelessWidget {
             FlatButton.icon(
               icon: Icon(Icons.add_box_rounded),
               label: Text(''),
-              onPressed: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> MyApp())),
+              onPressed: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => MyApp())),
             ),
           ],
         ),
-
         body: SafeArea(
           child: Column(
-
             children: [
               Container(
                 decoration: BoxDecoration(
                     image: DecorationImage(
-                        image: NetworkImage(
-                            "add you image URL here "
-                        ),
-                        fit: BoxFit.cover
-                    )
-                ),
+                        image: NetworkImage("add you image URL here "),
+                        fit: BoxFit.cover)),
                 child: Container(
                   width: double.infinity,
                   height: 200,
                   child: Container(
-                    alignment: Alignment(0.0,2.5),
+                    alignment: Alignment(0.0, 2.5),
                     child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          "Add you profile DP image URL here "
-                      ),
+                      backgroundImage:
+                          NetworkImage("Add you profile DP image URL here "),
                       radius: 60.0,
                     ),
                   ),
@@ -68,65 +44,62 @@ class ProfileScreen extends StatelessWidget {
                 height: 60,
               ),
               Text(
-                "Waleed"
-                ,style: TextStyle(
-                  fontSize: 25.0,
-                  color:Colors.grey,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w400
-              ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Malaysia, Johor bahru"
-                ,style: TextStyle(
-                  fontSize: 18.0,
-                  color:Colors.black45,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w300
-              ),
+                "Waleed",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w400),
               ),
               SizedBox(
                 height: 10,
               ),
               Text(
-                "App Developer at UTM "
-                ,style: TextStyle(
-                  fontSize: 15.0,
-                  color:Colors.black45,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w300
+                "Malaysia, Johor bahru",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
               ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "App Developer at UTM ",
+                style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
               ),
               SizedBox(
                 height: 10,
               ),
               Card(
-                  margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
+                  margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
                   elevation: 2.0,
                   child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
-                      child: Text("Details ",style: TextStyle(
-                          letterSpacing: 2.0,
-                          fontWeight: FontWeight.w300
-                      ),))
-              ),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+                      child: Text(
+                        "Details ",
+                        style: TextStyle(
+                            letterSpacing: 2.0, fontWeight: FontWeight.w300),
+                      ))),
               SizedBox(
                 height: 15,
               ),
               Text(
-                "Gender : Male  ||  Age : 23"
-                ,style: TextStyle(
-                  fontSize: 18.0,
-                  color:Colors.black45,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.w300
-              ),
+                "Gender : Male  ||  Age : 23",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
               ),
               Card(
-                margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Row(
@@ -135,43 +108,46 @@ class ProfileScreen extends StatelessWidget {
                       Expanded(
                         child: Column(
                           children: [
-                            Text("Medicines bought",
+                            Text(
+                              "Medicines bought",
                               style: TextStyle(
                                   color: Colors.blueAccent,
                                   fontSize: 22.0,
-                                  fontWeight: FontWeight.w600
-                              ),),
+                                  fontWeight: FontWeight.w600),
+                            ),
                             SizedBox(
                               height: 7,
                             ),
-                            Text("7",
+                            Text(
+                              "7",
                               style: TextStyle(
                                   color: Colors.black,
                                   fontSize: 22.0,
-                                  fontWeight: FontWeight.w300
-                              ),)
+                                  fontWeight: FontWeight.w300),
+                            )
                           ],
                         ),
                       ),
                       Expanded(
-                        child:
-                        Column(
+                        child: Column(
                           children: [
-                            Text("Illnesses",
+                            Text(
+                              "Illnesses",
                               style: TextStyle(
                                   color: Colors.blueAccent,
                                   fontSize: 22.0,
-                                  fontWeight: FontWeight.w600
-                              ),),
+                                  fontWeight: FontWeight.w600),
+                            ),
                             SizedBox(
                               height: 7,
                             ),
-                            Text("None",
+                            Text(
+                              "None",
                               style: TextStyle(
                                   color: Colors.black,
                                   fontSize: 22.0,
-                                  fontWeight: FontWeight.w300
-                              ),)
+                                  fontWeight: FontWeight.w300),
+                            )
                           ],
                         ),
                       ),
@@ -186,9 +162,8 @@ class ProfileScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   RaisedButton(
-                    onPressed: (){
-                    },
-                    shape:  RoundedRectangleBorder(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(80.0),
                     ),
                     child: Ink(
@@ -196,12 +171,14 @@ class ProfileScreen extends StatelessWidget {
                         gradient: LinearGradient(
                             begin: Alignment.centerLeft,
                             end: Alignment.centerRight,
-                            colors: [Colors.pink,Colors.redAccent]
-                        ),
+                            colors: [Colors.pink, Colors.redAccent]),
                         borderRadius: BorderRadius.circular(30.0),
                       ),
                       child: Container(
-                        constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
+                        constraints: BoxConstraints(
+                          maxWidth: 100.0,
+                          maxHeight: 40.0,
+                        ),
                         alignment: Alignment.center,
                         child: Text(
                           "Contact me",
@@ -209,16 +186,17 @@ class ProfileScreen extends StatelessWidget {
                               color: Colors.white,
                               fontSize: 12.0,
                               letterSpacing: 2.0,
-                              fontWeight: FontWeight.w300
-                          ),
+                              fontWeight: FontWeight.w300),
                         ),
                       ),
                     ),
                   ),
                   RaisedButton(
-                    onPressed: (){ Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> Home()));
+                    onPressed: () {
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Home()));
                     },
-                    shape:  RoundedRectangleBorder(
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(80.0),
                     ),
                     child: Ink(
@@ -226,12 +204,14 @@ class ProfileScreen extends StatelessWidget {
                         gradient: LinearGradient(
                             begin: Alignment.centerLeft,
                             end: Alignment.centerRight,
-                            colors: [Colors.pink,Colors.redAccent]
-                        ),
+                            colors: [Colors.pink, Colors.redAccent]),
                         borderRadius: BorderRadius.circular(80.0),
                       ),
                       child: Container(
-                        constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
+                        constraints: BoxConstraints(
+                          maxWidth: 100.0,
+                          maxHeight: 40.0,
+                        ),
                         alignment: Alignment.center,
                         child: Text(
                           "Portfolio",
@@ -239,8 +219,7 @@ class ProfileScreen extends StatelessWidget {
                               color: Colors.white,
                               fontSize: 12.0,
                               letterSpacing: 2.0,
-                              fontWeight: FontWeight.w300
-                          ),
+                              fontWeight: FontWeight.w300),
                         ),
                       ),
                     ),
@@ -249,8 +228,6 @@ class ProfileScreen extends StatelessWidget {
               )
             ],
           ),
-        )
-    );
+        ));
   }
 }
-
