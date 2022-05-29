4e2175879e3346e20ad7c7a1b71df338387d32ad
akram-tello <a.tello@graduate.utm.my>
2022-05-29 15:15:49 +0800

    refactor register page

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,17 +1,16 @@
 import 'package:signfun/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:signfun/main.dart';
-class Register extends StatefulWidget {
 
+class Register extends StatefulWidget {
   final Function toggleView;
-  Register({ required this.toggleView });
+  Register({required this.toggleView});
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
   String error = '';
@@ -32,7 +31,8 @@ class _RegisterState extends State<Register> {
           FlatButton.icon(
             icon: Icon(Icons.person),
             label: Text('Sign In'),
-            onPressed: () => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> MyApp())),
+            onPressed: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => MyApp())),
           ),
         ],
       ),
@@ -45,11 +45,9 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20.0),
               TextFormField(
                 decoration: const InputDecoration(
-                    icon:Icon(Icons.lock),
+                    icon: Icon(Icons.lock),
                     hintText: "Write Your Email Here ",
-                    labelText: 'Email'
-                ),
-
+                    labelText: 'Email'),
                 validator: (val) => val!.isEmpty ? 'Enter an email' : null,
                 onChanged: (val) {
                   setState(() => email = val);
@@ -58,69 +56,45 @@ class _RegisterState extends State<Register> {
               SizedBox(height: 20.0),
               TextFormField(
                 decoration: const InputDecoration(
-                  icon:Icon(Icons.lock),
-                  hintText: "Password",
-                  labelText: 'Password'
-                ),
+                    icon: Icon(Icons.lock),
+                    hintText: "Password",
+                    labelText: 'Password'),
                 obscureText: true,
-                validator: (val) => val!.length < 6 ? 'Enter a password 6+ chars long' : null,
+                validator: (val) =>
+                    val!.length < 6 ? 'Enter a password 6+ chars long' : null,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
               ),
               SizedBox(height: 20.0),
-
               Container(
                 width: double.infinity,
-                child : RawMaterialButton(
+                child: RawMaterialButton(
                   fillColor: Colors.pinkAccent,
                   elevation: 0.0,
                   padding: const EdgeInsets.symmetric(vertical: 20.0),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-                  onPressed: ()   async {
-                    if(_formKey.currentState!.validate()){
-                      dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-                      if(result == null) {
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
                         setState(() {
                           error = 'Please supply a valid email';
                         });
                       }
                     }
                   },
-                  child: Text('Sign Up',
-                    style:  TextStyle(
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
                       color: Colors.white,
                       fontSize: 18,
                     ),
                   ),
-
                 ),
-
               ),
-              // RaisedButton(
-              //     color: Colors.pink[400],
-              //     child: Text(
-              //       'Register',
-              //       style: TextStyle(
-              //         color: Colors.white,
-              //         fontSize: 18,
-              //       ),
-              //     ),
-              //
-              //
-              //
-              //
-              //     onPressed: () async {
-              //       if(_formKey.currentState!.validate()){
-              //         dynamic result = await _auth.registerWithEmailAndPassword(email, password);
-              //         if(result == null) {
-              //           setState(() {
-              //             error = 'Please supply a valid email';
-              //           });
-              //         }
-              //       }
-              //     }
-              // ),
               SizedBox(height: 12.0),
               Text(
                 error,
