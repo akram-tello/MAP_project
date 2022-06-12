7d324b2c72af5c05a9e74c6b0ed0d3033bd135e4
akram-tello <a.tello@graduate.utm.my>
2022-06-12 16:38:53 +0800

    add medicine by doctor and store it in firestore

---
+++ lib/addpost.dart
@@ -0,0 +1,99 @@
+import 'posts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+class create extends StatelessWidget {
+  TextEditingController Medicine = TextEditingController();
+  TextEditingController Price = TextEditingController();
+  TextEditingController Description = TextEditingController();
+
+  CollectionReference ref = FirebaseFirestore.instance.collection('posts');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PharmaX'),
+        backgroundColor: Colors.pinkAccent,
+        actions: [
+          MaterialButton(
+            onPressed: () {
+              ref.add({
+                'Medicine': Medicine.text,
+                'Price': Price.text,
+                'Description' : Description.text,
+              }).whenComplete(() {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => posts()));
+              });
+            },
+            child: Text(
+              "Create",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(border: Border.symmetric()),
+              child: TextField(
+                controller: Medicine,
+                decoration: InputDecoration(
+                  hintText: 'Add a New Medicine',
+                ),
+              ),
+
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(border: Border.symmetric()),
+                child: TextField(
+                  controller: Price,
+                  expands: false,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: 'Add The Price',
+                  ),
+                ),
+
+              ),
+
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(border: Border.symmetric()),
+                child: TextField(
+                  controller: Description,
+                  expands: false,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                    hintText: 'Medicine Description',
+                  ),
+                ),
+
+
+              ),
+
+            ),
+            SizedBox(
+              height: 250,
+            ),
+          ],
+
+
+        ),
+      ),
+    );
+  }
+}
